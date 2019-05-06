e5b3f8588d2e0df6b31282f77a1c21c32044d352
Nathan Beebe <njb99@drexel.edu>
2019-05-05 21:18:37 -0400

    Document Class, tokenize method, save_dictionary

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
 #Beginning of HW5
+
+#Grab 20 text instances in 20 .txt files
+
+class Document:
+
+    unique_words = {}
+
+    def tokenize(text):
+        words = text.split(" ",".")
+        for word in words:
+            if word not in unique_words:
+                insert = {word:1}
+                unique_words.update(insert)
+            else:
+                unique_words.update([word:1++])
+
+def save_dictionary(dict,pathname):
+    write_file = open("dictionary.txt","w")
+    for pair in dict:
+        write_file.write(pair + "\t")
+
+
+#produce TF and IDF stats
+def vectorize(path):
+
