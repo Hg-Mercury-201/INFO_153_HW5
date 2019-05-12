0eea14972eacb98a3ece56725e4ef0b5e0d7e346
Nathan Beebe <njb99@drexel.edu>
2019-05-11 21:01:27 -0400

    tested and fixed tokenize, save_dictionary, and vectorize

--- main.py
+++ main.py
@@ -6,37 +6,38 @@ class Document:
     @staticmethod
     def tokenize(text):
         unique_words = {}
-        #text.strip(";\".,/()[]!@#$%^&*()_")
+        text.strip(";\".,/()[]!@#$%^&*()_`~:")
         words = text.upper().split()
-        print(words)
+        #print(words)
         for word in words:
             if word not in unique_words:
                 new_word = {word:1}
                 unique_words.update(new_word)
             else:
                 unique_words[word] += 1
-        print(unique_words)
+        return(unique_words)
 
 
 def save_dictionary(dict,pathname):
-    write_file = open("dictionary.txt","w")
+    write_file = open(pathname,"w")
     for pair in dict:
-        write_file.write(pair + "\t")
+        write_file.write(pair + "\t"+ str(dict.get(pair)) + "\n")
 
 #produce TF and IDF stats
-def vectorize(path):
+def vectorize(pathname):
     DF_dictionary = {}
-    r = open(path,"r")
-    words = r.readlines()
-    save_dictionary(DF_dictionary,out)
-    for word in words:
-        for checks in DF_dictionary:
-            if word not in DF_dictionary:
-                DF_dictionary.update({word:1})
-            elif word is checks:
-                DF_dictionary[word] += 1
-    save_dictionary(DF_dictionary,"DF_Dictionary.txt")
+    doc = Document()
+    r = open(pathname,"r")
+    words = r.read()
+    DF_dictionary.update(doc.tokenize(words))
+    save_dictionary(DF_dictionary,"tf_DOCID.txt")
+    for word in DF_dictionary:
+        if word not in DF_dictionary:
+            DF_dictionary.update({word:1})
+        elif word is DF_dictionary:
+            DF_dictionary[word] += 1
+    save_dictionary(DF_dictionary,"df.txt")
 
-doc = Document()
-doc.tokenize("This is the important text that is important to the project")
-        
+
+vectorize("input_text.txt")
+print("done")
